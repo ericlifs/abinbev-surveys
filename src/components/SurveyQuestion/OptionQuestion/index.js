b18e02e3febb17eeb23fd6c17f87905a24fa0de6
Eric Lifszyc <lifszyc.eric@gmail.com>
2020-04-28 12:15:50 -0300

    Refactor multiple options new value computation function

--- src/components/SurveyQuestion/OptionQuestion/index.js
+++ src/components/SurveyQuestion/OptionQuestion/index.js
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
 
+import INPUT_TYPES from 'config/inputTypes';
+
 import { CheckInputWrapper, CheckInput, CheckInputLabel } from './styled';
 
 export default function OptionQuestion({ question, onChange }) {
   const [values, setValues] = useState([]);
-  const isMultiple = question.type === 'MULTIPLE_OPTION'; 
+  const isMultiple = question.type === INPUT_TYPES.MULTIPLE_OPTION; 
   const fieldType = isMultiple ? 'checkbox' : 'radio';
   
+  const computeMultipleOptionsValue = newValue => {
+    if (values.includes(newValue)) {
+      return values.filter(value => value !== newValue);
+    }
+
+    return [...values, newValue];
+  }
 
   const onValueChange = ev => {
-    const selectedNewValue = ev.target.value;
-
-    if (isMultiple) {
-      if (values.includes(selectedNewValue)) {
-        const newValues = values.filter(value => value !== selectedNewValue);
-
-        setValues(newValues);
-        return onChange(question.name, newValues);
-      }
-
-      const newValues = [...values, selectedNewValue];
-      setValues(newValues);
-      onChange(question.name, newValues);
-    } else {
-      onChange(question.name, selectedNewValue);
-    }
+    const newValue = isMultiple ? computeMultipleOptionsValue(ev.target.value) : ev.target.value;
+    
+    setValues(newValue);
+    onChange(question.name, newValue);
   }
 
   return question.answers.map(answer => (
