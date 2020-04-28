b9ed08052d5d367f1eac70236435474b751ce769
Eric Lifszyc <lifszyc.eric@gmail.com>
2020-04-28 11:07:16 -0300

    Create input types constants file

--- src/components/SurveyQuestion/index.js
+++ src/components/SurveyQuestion/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import INPUT_TYPES from 'config/inputTypes';
+
 import { TextInput, CheckInputWrapper, CheckInput, CheckInputLabel } from './styled';
 
 export default function SurveyQuestion({ question }) {
   const getContentByQuestionType = () => {
     switch (question.type) {
-      case "TEXT":
+      case INPUT_TYPES.TEXT:
         return <TextInput type="text" />;
 
-      case "MULTIPLE_OPTION": 
-      case "SINGLE_OPTION":
+      case INPUT_TYPES.MULTIPLE_OPTION: 
+      case INPUT_TYPES.SINGLE_OPTION:
         const fieldType = question.type === 'MULTIPLE_OPTION' ? 'checkbox' : 'radio';
 
         return question.answers.map(answer => (
